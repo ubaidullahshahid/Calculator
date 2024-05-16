b9ec1a0fee8352a6a4fbd515f2f683d63206c5fe
ubaidullahshahid <ubaidullahshahid098@gmail.com>
2024-05-17 03:33:39 +0500

    add functionality

---
+++ app.js
@@ -0,0 +1,9 @@
+var input = document.querySelector(".search");
+var button = document.querySelectorAll(".button");
+
+console.log("button" , handleClick(button));
+
+function handleClick (button) {
+    this.value
+
+}
