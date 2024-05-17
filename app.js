409dbd498a3e1b4bc5e57bcad7e5679dd284e3b6
ubaidullahshahid <ubaidullahshahid098@gmail.com>
2024-05-18 00:11:51 +0500

    add calculating functionality

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var input = document.querySelector(".search");
 var button = document.querySelectorAll(".button");
 
-console.log("button" , handleClick(button));
-
-function handleClick (button) {
-    this.value
-
-}
+button.forEach((button) => {
+  button.addEventListener("click", () => {
+    if (button.value === "C") {
+      input.value = " ";
+    } else if (button.value === "De") {
+      input.value = input.value.slice(0, -1);
+    } else if (button.value === "=") {
+      input.value = eval(input.value);
+    } else if (button.value) {
+      input.value += button.value;
+    } else {
+      alert("please enter correct value");
+    }
+  });
+});
